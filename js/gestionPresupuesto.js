e42451609541857b8d9b1d4f5f4ce29f4a3a3e5c
ISMAEL CASANOVA ANTON <ismapopeye@gmail.com>
2022-10-09 20:58:13 +0200

    Practica semana 2 - Ismael Casanova Anton
    realizacion del archivo gestionPresupuesto.js

--- js/gestionPresupuesto.js
+++ js/gestionPresupuesto.js
@@ -1,18 +1,41 @@
 // TODO: Crear las funciones, objetos y variables indicadas en el enunciado
 
 // TODO: Variable global
+let presupuesto = 0;
 
-
-function actualizarPresupuesto() {
+function actualizarPresupuesto(numero) {
     // TODO
+    if (numero > 0) {
+        presupuesto = numero;
+    } else {
+        return -1;
+    }
 }
 
 function mostrarPresupuesto() {
     // TODO
+    return "Tu presupuesto actual es de " + presupuesto + " €";
 }
 
 function CrearGasto() {
-    // TODO
+    var Gasto = {
+        descripcion: "",
+        valor: 0
+    };
+
+    function mostrarGasto() {
+        return "Gasto correspondiente a " + Gasto.descripcion + " con valor " + Gasto.valor + " €";
+    }
+
+    function actualizarDescripcion(dato) {
+        Gasto.descripcion = dato;
+    }
+
+    function actualizarValor(dato) {
+        if (dato > 0) {
+            Gasto.valor = dato;
+        }
+    }
 }
 
 // NO MODIFICAR A PARTIR DE AQUÍ: exportación de funciones y objetos creados para poder ejecutar los tests.
